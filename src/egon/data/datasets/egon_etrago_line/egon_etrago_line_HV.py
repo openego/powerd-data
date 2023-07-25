92ef1ab5979b5b73b63d014eff704dac51b7a227
Lennart Zimmermann <lennartzimmermann@outlook.de>
2023-07-25 14:34:52 +0200

    updating table, editing codes for searching for matches in hv database and mv database

--- src/egon/data/datasets/egon_etrago_line/egon_etrago_line_HV.py
+++ src/egon/data/datasets/egon_etrago_line/egon_etrago_line_HV.py
@@ -34,39 +34,17 @@ substation_df = gpd.read_postgis(
 
 
 # Read the Destination file from CSV
-lines_df = pd.read_csv("./egon_etrago_line_new.csv")
+lines_df = pd.read_csv("./NEP_tables_V2 - first table25July2023.csv")
 
 existing_lines_df = pd.read_sql(
     """
-    SELECT line_id FROM grid.egon_etrago_line   
+    SELECT line_id FROM grid.egon_etrago_line  
+
     """
     , engine)
 
 
-# # Read the Destination file from pgAdmin4
-# lines_df = pd.read_sql(
-#     """
-#     SELECT * FROM grid.egon_etrago_line_new
-#     UNION
-#     SELECT * FROM grid.egon_etrago_line_new;
-    
-#     """
-#     , engine)
-
-# lines_df = gpd.read_postgis(
-#     """
-#     SELECT * FROM grid.egon_etrago_line_new
-#     UNION
-#     SELECT * FROM grid.egon_etrago_line_new;
-    
-#     """
-#     , engine)
-
-
-# best_match_start=None
-# best_match_end=None
-
-unique_line_id = 29300
+unique_line_id = existing_lines_df['line_id'].max()
 
 formatted_point_0 = None
 formatted_point_1 = None
@@ -75,12 +53,13 @@ for index, row in lines_df.iterrows():
     # Add Unique line id
     unique_line_id += 1
     lines_df.at[index, 'line_id'] = unique_line_id
-    
-    Startpunkt = str(row['Startpoint'])
-    Endpunkt = str(row['Endpoint'])
+
+    Startpunkt = str(row['Startpunkt'])
+    Endpunkt = str(row['Endpunkt'])
     
     # Match Similarity of Source & Destination files for Start point  
-    matching_rows_start = substation_df[substation_df['subst_name'].apply(lambda x: any(difflib.SequenceMatcher(None, word, Startpunkt).ratio() >= 1 for word in x.split()))]
+    #matching_rows_start = substation_df[substation_df['subst_name'].apply(lambda x: any(difflib.SequenceMatcher(None, word, Startpunkt).ratio() >= 0.9 for word in x.split()))]
+    matching_rows_start = substation_df[substation_df['subst_name'].str.contains(Startpunkt, case=False, na=False, regex=False)]
     if not matching_rows_start.empty:
         
         lines_df.at[index, 'bus0'] = matching_rows_start.iloc[0]['bus_id']
@@ -93,11 +72,14 @@ for index, row in lines_df.iterrows():
         lines_df.at[index, 'Coordinate0'] = formatted_point_0
 
         # Calculate the matching percentage
+        """
         matching_percentage_start = difflib.SequenceMatcher(None, Startpunkt, matching_rows_start.iloc[0]['subst_name']).ratio() * 100
         lines_df.at[index, 'matching1%'] = round(matching_percentage_start,2)
-        
+        """
+
     # Match Similarity of Source & Destination files for End point                           
-    matching_rows_end = substation_df[substation_df['subst_name'].apply(lambda x: any(difflib.SequenceMatcher(None, word, Endpunkt).ratio() >= 1   for word in x.split()))]
+    #matching_rows_end = substation_df[substation_df['subst_name'].apply(lambda x: any(difflib.SequenceMatcher(None, word, Endpunkt).ratio() >= 0.9  for word in x.split()))]
+    matching_rows_end = substation_df[substation_df['subst_name'].str.contains(Endpunkt, case=False, na=False, regex=False)]
     if not matching_rows_end.empty:
         
         lines_df.at[index, 'bus1'] = matching_rows_end.iloc[0]['bus_id']
@@ -109,28 +91,18 @@ for index, row in lines_df.iterrows():
         lines_df.at[index, 'Coordinate1'] = formatted_point_1
 
         # Calculate the matching percentage
+        """
         matching_percentage_end = difflib.SequenceMatcher(None, Endpunkt, matching_rows_end.iloc[0]['subst_name']).ratio() * 100
         lines_df.at[index, 'matching2%'] = round(matching_percentage_end,2)
-
+        """
+        """
         if pd.notna(formatted_point_0) and pd.notna(formatted_point_1):
             lon0, lat0 = map(float, formatted_point_0.split(' '))
             lon1, lat1 = map(float, formatted_point_1.split(' '))
             distance = geodesic((lat0, lon0), (lat1, lon1)).kilometers
             lines_df.at[index, 'length'] = distance
-                    
-    best_match_end = None
-    best_match_start = None
-
-# buildyear_df = pd.read_csv("/home/student/Documents/Powerd/NEP_tables_finalVersion.csv")
-# buildyear_df['anvisierte Inbetriebnahme'] = buildyear_df["anvisierte Inbetriebnahme"].fillna('').str.split(r'[,/-]').apply(lambda x: x[1].strip() if len(x) > 1 else x[0].strip())
-
-# for index1, row1 in lines_df.iterrows():
-#     scn_name1 = str(row1['scn_name'])
-#     for index2, row2 in buildyear_df.iterrows():
-#         scn_name2 = str(row2['scn_name'])
-#         if scn_name1 == scn_name2:
-#             lines_df.loc[index1, 'build_year'] = row2['anvisierte Inbetriebnahme']
+        """            
 
 # Save the updated file
-lines_df.to_csv('./egon_etrago_line_new.csv', index=False)
+lines_df.to_csv('./NEP_tables_V2 - first table25July2023_test.csv', index=False)
 print("Operation successful")
--- src/egon/data/datasets/egon_etrago_line/egon_etrago_line_MV.py
+++ src/egon/data/datasets/egon_etrago_line/egon_etrago_line_MV.py
@@ -17,8 +17,7 @@ engine = create_engine(
 # Read the Source file
 substation_df = pd.read_sql(
     """
-    SELECT * FROM grid.egon_ehv_substation
-    UNION
+
     SELECT * FROM grid.egon_hvmv_substation;
     
     """
@@ -28,8 +27,7 @@ substation_df = pd.read_sql(
 
 substation_df = gpd.read_postgis(
     """
-    SELECT * FROM grid.egon_ehv_substation
-    UNION
+  
     SELECT * FROM grid.egon_hvmv_substation;
     
     """
@@ -38,7 +36,7 @@ substation_df = gpd.read_postgis(
 
 
 # Read the Destination file from CSV
-lines_df = pd.read_csv("./egon_etrago_line_new.csv")
+lines_df = pd.read_csv("./NEP_tables_V2 - first table25July2023_test.csv")
 
 existing_lines_df = pd.read_sql(
     """
@@ -47,31 +45,8 @@ existing_lines_df = pd.read_sql(
     , engine)
 
 
-# # Read the Destination file from pgAdmin4
-# lines_df = pd.read_sql(
-#     """
-#     SELECT * FROM grid.egon_etrago_line_new
-#     UNION
-#     SELECT * FROM grid.egon_etrago_line_new;
-    
-#     """
-#     , engine)
-
-# lines_df = gpd.read_postgis(
-#     """
-#     SELECT * FROM grid.egon_etrago_line_new
-#     UNION
-#     SELECT * FROM grid.egon_etrago_line_new;
-    
-#     """
-#     , engine)
-
-
 # Match Similarity of Source & Destination files 
-best_match_start=None
-best_match_end=None
-
-unique_line_id = 29300
+unique_line_id = existing_lines_df['line_id'].max()
 
 formatted_point_0 = None
 formatted_point_1 = None
@@ -81,36 +56,37 @@ for index, row in lines_df.iterrows():
     unique_line_id += 1
     lines_df.at[index, 'line_id'] = unique_line_id
     
-    Startpunkt = str(row['Startpoint'])
-    Endpunkt = str(row['Endpoint'])
+    Startpunkt = str(row['Startpunkt'])
+    Endpunkt = str(row['Endpunkt'])
     
-    # for r in [1.0, 0.9, 0.8, 0.7, 0.6, 0.5]:
-        
-    #     if best_match_start is None:
-    #         matching_rows_start = substation_df[substation_df['subst_name'].apply(
-    #             lambda x: any(difflib.SequenceMatcher(None, word, Startpunkt).ratio() >= r for word in x.split()))]
-    matching_rows_start = substation_df[substation_df['subst_name'].apply(lambda x: any(difflib.SequenceMatcher(None, word, Startpunkt).ratio() >= 1 for word in x.split()))]
-    if not matching_rows_start.empty:
-        best_match_start = 1
-        
-        lines_df.at[index, 'bus0'] = matching_rows_start.iloc[0]['bus_id']
-        lines_df.at[index, 'subst_name0'] = matching_rows_start.iloc[0]['subst_name']
+    # Only when bus_id is not filled up yet
+    if pd.isna(row['bus0']):   
+        #matching_rows_start = substation_df[substation_df['subst_name'].apply(lambda x: any(difflib.SequenceMatcher(None, word, Startpunkt).ratio() >= 0.9 for word in x.split()))]
+        matching_rows_start = substation_df[substation_df['subst_name'].str.contains(Startpunkt, case=False, na=False, regex=False)]
+       
+        if not matching_rows_start.empty:
+            lines_df.at[index, 'bus0'] = matching_rows_start.iloc[0]['bus_id']
+            lines_df.at[index, 'subst_name0'] = matching_rows_start.iloc[0]['subst_name']
 
-        
-        # Find coordinate for start point
-        point_0 = matching_rows_start.iloc[0]['point']
-        formatted_point_0 = f"{point_0.x} {point_0.y}"
-        lines_df.at[index, 'Coordinate0'] = formatted_point_0
+            # Find coordinate for start point
+            point_0 = matching_rows_start.iloc[0]['point']
+            formatted_point_0 = f"{point_0.x} {point_0.y}"
+            lines_df.at[index, 'Coordinate0'] = formatted_point_0
 
-        # Calculate the matching percentage
-        matching_percentage_start = difflib.SequenceMatcher(None, Startpunkt, matching_rows_start.iloc[0]['subst_name']).ratio() * 100
-        lines_df.at[index, 'matching1%'] = round(matching_percentage_start,2) 
-                
-        # if best_match_end is None:
-        #     matching_rows_end = substation_df[substation_df['subst_name'].apply(
-        #         lambda x: any(difflib.SequenceMatcher(None, word, Endpunkt).ratio() >= r for word in x.split()))]
-            
-        matching_rows_end = substation_df[substation_df['subst_name'].apply(lambda x: any(difflib.SequenceMatcher(None, word, Endpunkt).ratio() >= 1   for word in x.split()))]
+            """
+            # Calculate the matching percentage
+            matching_percentage_start = difflib.SequenceMatcher(None, Startpunkt, matching_rows_start.iloc[0]['subst_name']).ratio() * 100
+            lines_df.at[index, 'matching1%'] = round(matching_percentage_start,2) 
+                    
+            # if best_match_end is None:
+            #     matching_rows_end = substation_df[substation_df['subst_name'].apply(
+            #         lambda x: any(difflib.SequenceMatcher(None, word, Endpunkt).ratio() >= r for word in x.split()))]
+            """
+    
+    if pd.isna(row['bus1']):
+        #matching_rows_end = substation_df[substation_df['subst_name'].apply(lambda x: any(difflib.SequenceMatcher(None, word, Endpunkt).ratio() >= 0.9   for word in x.split()))]
+        matching_rows_end = substation_df[substation_df['subst_name'].str.contains(Endpunkt, case=False, na=False, regex=False)]
+        
         if not matching_rows_end.empty:
             best_match_end = 1
             lines_df.at[index, 'bus1'] = matching_rows_end.iloc[0]['bus_id']
@@ -121,29 +97,20 @@ for index, row in lines_df.iterrows():
             formatted_point_1 = f"{point_1.x} {point_1.y}"
             lines_df.at[index, 'Coordinate1'] = formatted_point_1
 
+            """
             # Calculate the matching percentage
             matching_percentage_end = difflib.SequenceMatcher(None, Endpunkt, matching_rows_end.iloc[0]['subst_name']).ratio() * 100
             lines_df.at[index, 'matching2%'] = round(matching_percentage_end,2)
+            """
 
+            """
             if pd.notna(formatted_point_0) and pd.notna(formatted_point_1):
                 lon0, lat0 = map(float, formatted_point_0.split(' '))
                 lon1, lat1 = map(float, formatted_point_1.split(' '))
                 distance = geodesic((lat0, lon0), (lat1, lon1)).kilometers
                 lines_df.at[index, 'length'] = distance
-                    
-    best_match_end = None
-    best_match_start = None
-
-# buildyear_df = pd.read_csv("/home/student/Documents/Powerd/NEP_tables_finalVersion.csv")
-# buildyear_df['anvisierte Inbetriebnahme'] = buildyear_df["anvisierte Inbetriebnahme"].fillna('').str.split(r'[,/-]').apply(lambda x: x[1].strip() if len(x) > 1 else x[0].strip())
-
-# for index1, row1 in lines_df.iterrows():
-#     scn_name1 = str(row1['scn_name'])
-#     for index2, row2 in buildyear_df.iterrows():
-#         scn_name2 = str(row2['scn_name'])
-#         if scn_name1 == scn_name2:
-#             lines_df.loc[index1, 'build_year'] = row2['anvisierte Inbetriebnahme']
+            """
 
 # Save the updated file
-lines_df.to_csv('./egon_etrago_line_new.csv', index=False)
+lines_df.to_csv('./NEP_tables_V2 - first table25July2023_test.csv', index=False)
 print("Operation successful")
