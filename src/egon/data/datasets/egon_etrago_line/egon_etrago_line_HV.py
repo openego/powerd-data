98e70f5f76c25c338fadd696fb93158d4fd0f1e6
smh.sadat <smh.sadat1989@gmail.com>
2023-07-31 00:45:05 +0430

    work started from 50 up to 91

--- src/egon/data/datasets/egon_etrago_line/egon_etrago_line_HV.py
+++ src/egon/data/datasets/egon_etrago_line/egon_etrago_line_HV.py
@@ -40,12 +40,12 @@ existing_lines_df = pd.read_sql(
 
 # Read the Destination file from CSV
 lines_df = pd.read_csv("./NEP_tables_V2 - first table26July2023 - test.csv")
+
 unique_line_id = existing_lines_df['line_id'].max()
+formatted_point_0 = None
+formatted_point_1 = None
 
 for index, row in lines_df.iterrows():
-   
-    formatted_point_0 = None
-    formatted_point_1 = None
     # Add Unique line id
     unique_line_id += 1
     lines_df.at[index, 'line_id'] = unique_line_id
@@ -62,22 +62,15 @@ for index, row in lines_df.iterrows():
         if pd.isnull(lines_df.at[index, 'subst_name0']):
             lines_df.at[index, 'subst_name0'] = {matching_rows_start.iloc[0]['subst_name']}
 
-        
         # Find coordinate for start point
         if pd.isnull(lines_df.at[index, 'Coordinate0']):
             point_0 = matching_rows_start.iloc[0]['point']
             formatted_point_0 = f"{point_0.x} {point_0.y}"
             lines_df.at[index, 'Coordinate0'] = formatted_point_0    
-        else : # #just for checking if the geodata from subst_point is right (compare length from pdf with calculation)
-            x, y = map(float, row['Coordinate0'].split())
-            point_0 = sg.Point(x,y)
-            formatted_point_0 = f"{point_0.x} {point_0.y}"
-
 
         # Calculate the matching percentage
         matching_percentage_start = difflib.SequenceMatcher(None, Startpunkt, matching_rows_start.iloc[0]['subst_name']).ratio() * 100
         lines_df.at[index, 'matching0%'] = round(matching_percentage_start,0)
-        
 
     # Match Similarity of Source & Destination files for End point                           
     matching_rows_end = substation_df[substation_df['subst_name'].str.contains(Endpunkt, case=False, na=False, regex=False)]
@@ -88,34 +81,25 @@ for index, row in lines_df.iterrows():
         if pd.isnull(lines_df.at[index, 'subst_name1']):
             lines_df.at[index, 'subst_name1'] = matching_rows_end.iloc[0]['subst_name']
 
-        
-        # Find coordinate for end point
+        # Find coordinate for End point
         if pd.isnull(lines_df.at[index, 'Coordinate1']):
             point_1 = matching_rows_end.iloc[0]['point']
             formatted_point_1 = f"{point_1.x} {point_1.y}"
             lines_df.at[index, 'Coordinate1'] = formatted_point_1
-        else : # #just for checking if the geodata from subst_point is right (compare length from pdf with calculation)
-            x, y = map(float, row['Coordinate1'].split())
-            point_1 = sg.Point(x,y)
-            formatted_point_1 = f"{point_1.x} {point_1.y}"
 
         # Calculate the matching percentage
         matching_percentage_end = difflib.SequenceMatcher(None, Endpunkt, matching_rows_end.iloc[0]['subst_name']).ratio() * 100
         lines_df.at[index, 'matching1%'] = round(matching_percentage_end,0)
 
         # Calculate lenght of Transmission Line
-        if pd.notna(formatted_point_0) and pd.notna(formatted_point_1):
-            if pd.isnull(lines_df.at[index, 'length']) :
-                lon0, lat0 = map(float, formatted_point_0.split(' '))
-                lon1, lat1 = map(float, formatted_point_1.split(' '))
+        if pd.notna(lines_df.at[index, 'Coordinate0']) and pd.notna(lines_df.at[index, 'Coordinate1']):
+            if pd.isnull(lines_df.at[index, 'length']):
+                coordinate_0_str = str(lines_df.at[index, 'Coordinate0'])
+                lon0, lat0 = map(float, coordinate_0_str.split(' '))
+                coordinate_1_str = str(lines_df.at[index, 'Coordinate1'])
+                lon1, lat1 = map(float, coordinate_1_str.split(' '))
                 distance = geodesic((lat0, lon0), (lat1, lon1)).kilometers
                 lines_df.at[index, 'length'] = f'HV {round(distance*1.1,1)}'
-                lines_df.at[index, 'length_calculated'] = f'HV {round(distance*1.1,1)}'
-            else :   #just for checking if the geodata from subst_point is right
-                lon0, lat0 = map(float, formatted_point_0.split(' '))
-                lon1, lat1 = map(float, formatted_point_1.split(' '))
-                distance = geodesic((lat0, lon0), (lat1, lon1)).kilometers
-                lines_df.at[index, 'length_calculated'] = f'HV {round(distance*1.1,1)}'
 
 # Save the updated file
 lines_df.to_csv('./NEP_tables_V2 - first table26July2023 - test.csv', index=False)
--- src/egon/data/datasets/egon_etrago_line/egon_etrago_line_transfer_buses.py
+++ src/egon/data/datasets/egon_etrago_line/egon_etrago_line_transfer_buses.py
@@ -39,7 +39,6 @@ existing_lines_df = pd.read_sql(
 # Read the Destination file from CSV
 lines_df = pd.read_csv("./NEP_tables_V2 - first table26July2023 - test.csv")
 
-
 unique_line_id = existing_lines_df['line_id'].max()
 formatted_point_0 = None
 formatted_point_1 = None
@@ -59,19 +58,18 @@ for index, row in lines_df.iterrows():
         if pd.isnull(lines_df.at[index, 'bus0']):
             lines_df.at[index, 'bus0'] = matching_rows_start.iloc[0]['bus_id']
         if pd.isnull(lines_df.at[index, 'subst_name0']):
-            lines_df.at[index, 'subst_name0'] = matching_rows_start.iloc[0]['subst_name']
-
+            lines_df.at[index, 'subst_name0'] = {matching_rows_start.iloc[0]['subst_name']}
         
         # Find coordinate for start point
-        point_0 = matching_rows_start.iloc[0]['point']
-        formatted_point_0 = f"{point_0.x} {point_0.y}"
-        lines_df.at[index, 'Coordinate0'] = formatted_point_0
+        if pd.isnull(lines_df.at[index, 'Coordinate0']):
+            point_0 = matching_rows_start.iloc[0]['point']
+            formatted_point_0 = f"{point_0.x} {point_0.y}"
+            lines_df.at[index, 'Coordinate0'] = formatted_point_0 
 
         # Calculate the matching percentage
         matching_percentage_start = difflib.SequenceMatcher(None, Startpunkt, matching_rows_start.iloc[0]['subst_name']).ratio() * 100
         lines_df.at[index, 'matching0%'] = round(matching_percentage_start,0)
         
-
     # Match Similarity of Source & Destination files for End point                           
     matching_rows_end = substation_df[substation_df['subst_name'].str.contains(Endpunkt, case=False, na=False, regex=False)]
     if not matching_rows_end.empty:
@@ -81,20 +79,23 @@ for index, row in lines_df.iterrows():
         if pd.isnull(lines_df.at[index, 'subst_name1']):
             lines_df.at[index, 'subst_name1'] = matching_rows_end.iloc[0]['subst_name']
 
-        # Find coordinate for end point
-        point_1 = matching_rows_end.iloc[0]['point']
-        formatted_point_1 = f"{point_1.x} {point_1.y}"
-        lines_df.at[index, 'Coordinate1'] = formatted_point_1
-
+        # Find coordinate for End point
+        if pd.isnull(lines_df.at[index, 'Coordinate1']):
+            point_1 = matching_rows_end.iloc[0]['point']
+            formatted_point_1 = f"{point_1.x} {point_1.y}"
+            lines_df.at[index, 'Coordinate1'] = formatted_point_1
+            
         # Calculate the matching percentage
         matching_percentage_end = difflib.SequenceMatcher(None, Endpunkt, matching_rows_end.iloc[0]['subst_name']).ratio() * 100
         lines_df.at[index, 'matching1%'] = round(matching_percentage_end,0)
 
         # Calculate lenght of Transmission Line
-        if pd.notna(formatted_point_0) and pd.notna(formatted_point_1):
+        if pd.notna(lines_df.at[index, 'Coordinate0']) and pd.notna(lines_df.at[index, 'Coordinate1']):
             if pd.isnull(lines_df.at[index, 'length']):
-                lon0, lat0 = map(float, formatted_point_0.split(' '))
-                lon1, lat1 = map(float, formatted_point_1.split(' '))
+                coordinate_0_str = str(lines_df.at[index, 'Coordinate0'])
+                lon0, lat0 = map(float, coordinate_0_str.split(' '))
+                coordinate_1_str = str(lines_df.at[index, 'Coordinate1'])
+                lon1, lat1 = map(float, coordinate_1_str.split(' '))
                 distance = geodesic((lat0, lon0), (lat1, lon1)).kilometers
                 lines_df.at[index, 'length'] = f'TB {round(distance*1.1,1)}'
 
