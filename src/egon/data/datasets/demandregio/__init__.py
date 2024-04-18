43ee476d092b4e812b715bfcee51fb2fbc8d42b9
ClaraBuettner <clara.buettner@hs-flensburg.de>
2024-04-18 10:48:04 +0200

    Fix merge

--- src/egon/data/datasets/demandregio/__init__.py
+++ src/egon/data/datasets/demandregio/__init__.py
@@ -903,3 +903,20 @@ def timeseries_per_wz():
         print(year)
         for sector in ["CTS", "industry"]:
             insert_timeseries_per_wz(sector, int(year))
+
+
+def get_cached_tables():
+    """Get cached demandregio tables and db-dump from former runs"""
+    data_config = egon.data.config.datasets()
+    for s in ["cache", "dbdump"]:
+        url = data_config["demandregio_workaround"]["source"][s]["url"]
+        target_path = data_config["demandregio_workaround"]["targets"][s][
+            "path"
+        ]
+        filename = os.path.basename(url)
+        file_path = Path(".", target_path, filename).resolve()
+        os.makedirs(file_path.parent, exist_ok=True)
+        logger.info(f"Downloading: {filename} from {url}.")
+        download_and_check(url, file_path, max_iteration=5)
+        with zipfile.ZipFile(file_path, "r") as zip_ref:
+            zip_ref.extractall(file_path.parent)
