0f8b64721b993d5807e91cda4179f1aadb3bf518
ClaraBuettner <clara.buettner@hs-flensburg.de>
2024-06-12 09:43:53 +0200

    Fix outdated path names

--- src/egon/data/datasets/pypsaeur/__init__.py
+++ src/egon/data/datasets/pypsaeur/__init__.py
@@ -139,7 +139,7 @@ def solve_network():
 def read_network():
     if config.settings()["egon-data"]["--run-pypsa-eur"]:
         with open(
-            __path__[0] + "/datasets/pypsaeursec/config.yaml", "r"
+            __path__[0] + "/datasets/pypsaeur/config.yaml", "r"
         ) as stream:
             data_config = yaml.safe_load(stream)
 
@@ -1382,7 +1382,7 @@ def rual_heat_technologies(network):
 
 def execute():
     with open(
-        __path__[0] + "/datasets/pypsaeursec/config.yaml", "r"
+        __path__[0] + "/datasets/pypsaeur/config.yaml", "r"
     ) as stream:
         data_config = yaml.safe_load(stream)
 
