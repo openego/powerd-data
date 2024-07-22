48be9dcd591ef59d5edfa319e871a27bc073ec2e
Robert John <robert.john@ise.fraunhofer.de>
2024-07-22 08:28:05 +0200

    def _get_working_dir_mastr() due to setting path for local files not consitent, thus lookup in known paths.

--- src/egon/data/datasets/mastr.py
+++ src/egon/data/datasets/mastr.py
@@ -31,6 +31,59 @@ WORKING_DIR_MASTR_OLD = Path(".", "bnetza_mastr", "dump_2021-05-03")
 WORKING_DIR_MASTR_NEW = Path(".", "bnetza_mastr", "dump_2022-11-17")
 
 
+def _get_working_dir_mastr(target_file=None):
+    """get most actual mastr working dir if hardset data not existing in in paths
+        - WORKING_DIR_MASTR_OLD
+        - WORKING_DIR_MASTR_NEW
+
+    if target_file return path for it otherwise return most actual mastr working dir
+
+    Returns
+    -------
+    path for most actual mastr working dir or file if exist
+
+    """
+
+    # already given dir options
+    p_OLD = Path(".", "bnetza_mastr", "dump_2021-05-03")
+    p_NEW = Path(".", "bnetza_mastr", "dump_2022-11-17")
+
+    # collect all possible dir paths here if they exist
+    dirs_to_check_for_working_dir = [p for p in [p_NEW, p_OLD] if os.path.isdir(p)]
+
+    # get dirs to navigate more easily
+    powerd_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(
+        os.path.abspath(__file__))))))
+    execution_folder_dir = os.path.dirname(powerd_dir)  # execution_folder is parent of powerd-data
+    bnetza_mastr_dir = os.path.join(execution_folder_dir, "bnetza_mastr")  # bnetza_mastr in execution_folder
+    p_ALT = [d for d in os.listdir(bnetza_mastr_dir) if os.path.isdir(d)]  # keep directories only it they exist
+    p_ALT.sort(reverse=True)  # sort from most actual to oldest
+
+    dirs_to_check_for_working_dir += p_ALT  # check these dirs if they exist
+
+    assert dirs_to_check_for_working_dir, \
+        ("Looks like we don't have existing mastr working dirs; got "
+         f"dirs_to_check_for_working_dir: {dirs_to_check_for_working_dir}")
+
+    path_to_return = None
+    if target_file:
+        for dir in dirs_to_check_for_working_dir:
+            fn = os.path.join(dir, target_file)
+            if os.path.isfile(fn):
+                path_to_return = fn
+                break
+    else:
+        # due to dirs_to_check_for_working_dir is ordered
+        # take first one
+        path_to_return = dirs_to_check_for_working_dir[0]
+
+    assert path_to_return, \
+        ("Didn't find any existing mastr dir. Checking for a "
+         f"specific target_file is {target_file}. target_file: {target_file}")
+
+    return path_to_return
+
+
 def download_mastr_data():
     """Download MaStR data from Zenodo"""
 
