b5865aa137b5c40583143eb0c77995201acf835b
Robert John <robert.john@ise.fraunhofer.de>
2024-10-14 11:56:20 +0200

    2019 to status quo

--- src/egon/data/airflow/dags/pipeline_status_quo.py
+++ src/egon/data/airflow/dags/pipeline_status_quo.py
@@ -44,7 +44,7 @@ from egon.data.datasets.heat_demand_timeseries import HeatTimeSeries
 from egon.data.datasets.heat_etrago import HeatEtrago
 from egon.data.datasets.heat_etrago.hts_etrago import HtsEtragoTable
 from egon.data.datasets.heat_supply import HeatSupply
-from egon.data.datasets.heat_supply.individual_heating import HeatPumps2019
+from egon.data.datasets.heat_supply.individual_heating import HeatPumpsStatusQuo
 from egon.data.datasets.industrial_sites import MergeIndustrialSites
 from egon.data.datasets.industry import IndustrialDemandCurves
 from egon.data.datasets.loadarea import LoadArea, OsmLanduse
@@ -421,8 +421,8 @@ with airflow.DAG(
             scenario_capacities,
         ]
     )
-    
-    
+
+
     # Pumped hydro units
     pumped_hydro = Storages(
         dependencies=[
@@ -450,7 +450,7 @@ with airflow.DAG(
     chp_etrago = ChpEtrago(dependencies=[chp, heat_etrago])
 
     # Heat pump disaggregation for status2019
-    heat_pumps_2019 = HeatPumps2019(
+    heat_pumps_status_quo = HeatPumpsStatusQuo(
         dependencies=[
             cts_demand_buildings,
             DistrictHeatingAreas,
@@ -467,7 +467,7 @@ with airflow.DAG(
             heat_etrago,
             heat_time_series,
             mv_grid_districts,
-            heat_pumps_2019,
+            heat_pumps_status_quo,
         ]
     )
 
