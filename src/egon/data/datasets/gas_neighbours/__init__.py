134679788bf2a9cd7e2f73b89ade2d8ac8d090e3
Robert John <robert.john@ise.fraunhofer.de>
2024-11-05 09:34:56 +0100

    hotfix-no_gas_neighbours_required

--- src/egon/data/datasets/gas_neighbours/__init__.py
+++ src/egon/data/datasets/gas_neighbours/__init__.py
@@ -2,6 +2,7 @@
 """
 
 from egon.data.datasets import Dataset
+from egon.data import config
 from egon.data.datasets.gas_neighbours.eGon100RE import (
     insert_gas_neigbours_eGon100RE,
 )
@@ -13,11 +14,37 @@ from egon.data.datasets.gas_neighbours.eGon2035 import (
 )
 
 
+def no_gas_neighbours_required():
+    print(
+        """
+          None of the required scenarios need the creation of
+          foreign gas buses
+          """
+    )
+    return
+
+
+tasks = ()
+
+if "eGon2035" in config.settings()["egon-data"]["--scenarios"]:
+    tasks = tasks + (
+        tyndp_gas_generation,
+        tyndp_gas_demand,
+        grid,
+        insert_ocgt_abroad,
+    )
+if "eGon100RE" in config.settings()["egon-data"]["--scenarios"]:
+    tasks = tasks + (insert_gas_neigbours_eGon100RE,)
+
+if tasks == ():
+    tasks = tasks + (no_gas_neighbours_required,)
+
+
 class GasNeighbours(Dataset):
     def __init__(self, dependencies):
         super().__init__(
             name="GasNeighbours",
-            version="0.0.4",
+            version="0.0.5",
             dependencies=dependencies,
             tasks=(
                 tyndp_gas_generation,
