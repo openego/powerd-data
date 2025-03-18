3fa76537543a0b5c9fa7b8462b38e12206ec823e
Robert John <robert.john@ise.fraunhofer.de>
2025-03-18 10:53:01 +0100

    update version

--- src/egon/data/datasets/heat_demand_timeseries/__init__.py
+++ src/egon/data/datasets/heat_demand_timeseries/__init__.py
@@ -1096,7 +1096,7 @@ class HeatTimeSeries(Dataset):
     def __init__(self, dependencies):
         super().__init__(
             name="HeatTimeSeries",
-            version="0.0.8",
+            version="0.0.9",
             dependencies=dependencies,
             tasks=(
                 {
